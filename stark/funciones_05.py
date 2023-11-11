85b161a1d3d2877e5d65012a2c703e0fbf6e90a6
cosalamone <cosalamone@gmail.com>
2023-11-11 16:10:47 -0300

    stark_05 1.3

--- stark/funciones_05.py
+++ stark/funciones_05.py
@@ -7,7 +7,6 @@ def leer_archivo(nombre_archivo:str):
     '''
 Brief:
     Abre el archivo indicado por parametro, según la extensión que sea. Lo hace sólo en modo lectura
-    
 Parametros:
     nombre_archivo: str --> Corresponde al nombre del archivo y a la extension del mismo
 Retorno:
@@ -57,45 +56,57 @@ Iron Man,Tony Stark,Marvel,1.85,88.5,Hombre,Azul,Negro,6,sobresaliente
 Black Widow,Natasha Romanoff,Marvel,1.7,58.0,Mujer,Verde,Pelirrojo,7,alta
 '''
 
-print(guardar_archivo('./stark/data_stark.csv', info_a_guardar))
+# print(guardar_archivo('./stark/data_stark.csv', info_a_guardar))
+
 # 1.3
-def generar_csv(path:str, lista_heroe:list):
+def generar_csv(path, lista_heroe:list):
     '''
 Brief:
+    Genera un archivo csv en base a la lista recibida por parametro y la guarda en un archivo 
+    Primero toma los encabezados y seguido de ellos en las lineas siguientes se guarda la información. 
+    
 Parametros:
-    path: str --> Corresponde al nombre del archivo y a la extension del mismo
+    path: str --> Corresponde al nombre del archivo y a la extension del mismo donde guardará la información
+    lista_heroe: list
 Retorno:
-'''
-    lista_completa = []
-    retorno = None
+    De estar vacia la lista retorna False
+'''    
+    contenido = ''
+    heroe:dict
+    encabezado = True
     if len(lista_heroe) > 0:
-        with open(path, 'w+', encoding='utf-8') as archivo:
-            for linea in archivo:
-                lista = linea.split(',')
-                heroe = {}
-                heroe['nombre'] = lista[0]
-                heroe['identidad'] = lista[1]
-                heroe['empresa'] = lista[2]
-                heroe['altura'] = float(lista[3])
-                heroe['peso'] = float(lista[4])
-                heroe['genero'] = lista[5]
-                heroe['color_ojos'] = lista[6]
-                heroe['color_pelo'] = lista[7]
-                heroe['fuerza_'] = int(lista[8])
-                heroe['inteligencia'] = lista[9]
+        
+        for heroe in lista_heroe:
+            # en la primer vuelta tomo los encabezados
+            if encabezado == True:
+                lista = list(heroe.keys())
+                for key in lista:
+                    contenido += f'{key},'
+                encabezado = False
+                contenido += '\n'
+            else: 
+                # luego tomo los valores que componen a cada heroe
+                nombre = heroe['nombre']
+                identidad = heroe['identidad']
+                empresa = heroe['empresa']
+                altura = heroe['altura']
+                peso = heroe['peso']
+                fuerza = heroe['fuerza']
+                color_ojos = heroe['color_ojos']
+                color_pelo = heroe['color_pelo']
+                inteligencia = heroe['inteligencia']
 
-            lista_completa.append(heroe)
-            retorno = archivo.write(f'{lista_heroe}')
-            if retorno:
-                print(f'Se generó el archivo csv: "{path}"')
-                retorno = True
-            else:
-                print(f'Error al generar el archivo csv: "{path}"')
-                retorno = False
-    return retorno
+                contenido += f'{nombre},{identidad},{empresa},{altura},{peso},{fuerza},{color_ojos},{color_pelo},{inteligencia}\n'
+
+        guardar_archivo(path, contenido)
+    else:
+        return False
 
 
-# generar_csv('stark/data_stark.csv', lista_personajes)
+# generar_csv(lista_personajes)
+
+
+# 1.4
 
 '''
 Brief:
